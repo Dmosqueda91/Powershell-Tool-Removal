5a5be613b0cca457cc644697841f696474f9992a
dmosqueda-orbis <123583296+dmosqueda-orbis@users.noreply.github.com>
2023-01-25 09:54:39 -0800

    Add files via upload
    ThreatLocker removal script has been added to Repo.

---
+++ ThreatLocker Removal.ps1
@@ -0,0 +1,21 @@
+# This script will download installer and run the unistall that is within the file. 
+
+
+# Check if the operating system is 64-bit or 32-bit
+if ([Environment]::Is64BitOperatingSystem) {
+    # If 64-bit, set the download URL for the 64-bit installer
+    $downloadURL = "https://api.threatlocker.com/installers/threatlockerstubx64.exe";
+}
+else {
+    # If 32-bit, set the download URL for the 32-bit installer
+    $downloadURL = "https://api.threatlocker.com/installers/threatlockerstubx86.exe";
+}
+
+# Set the local installer file path
+$localInstaller = "C:\Temp\ThreatLockerStub.exe";
+
+# Download the installer from the specified URL
+Invoke-WebRequest -Uri $downloadURL -OutFile $localInstaller;
+
+# Run the installer with the "uninstall" parameter
+& $localInstaller uninstall;
